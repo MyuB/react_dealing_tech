40fa19faef059c4c73cd1790c3a0d0a6963ebfc8
MyuB <mjycoco0@naver.com>
2022-03-12 13:49:55 +0900

    ch7 lifecycle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import "./App.css";
-import IterationSample from "./IterationSample";
+import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 
-const App = () => {
-  return <IterationSample />;
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>random color</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
